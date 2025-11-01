8369d4e7e28e1458c526c654f2b11f184e42d647
senhorsulaiman <senhorsulaiman@gmail.com>
2025-11-02 00:36:54 +0400

    fix1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-export {default as HomeLayout } from './homeLayout'
+export {default as HomeLayout } from './HomeLayout'
 export {default as Landing } from './Landing'
 export {default as About } from './About'
 export {default as Listings } from './Listings'
